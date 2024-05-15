fa71bc655a0c99890ccb8805bba4ac9a47eb6e67
Emmanuel <emmaimade14@gmail.com>
2024-05-15 05:42:19 +0100

    chore: update withdrawal limit and get transactions features

--- bankAccount-system/index.js
+++ bankAccount-system/index.js
@@ -1,106 +1,3 @@
-class BankAccount {
-    constructor(accountNumber, firstName, lastName, balance, dailyWithdrawalLimit) {
-        this._accountNumber = accountNumber;
-        this._firstName = firstName;
-        this._lastName = lastName;
-        this._accountHolder = `${firstName} ${lastName}`;
-        this._balance = balance;
-        this._transactions = [];
-        this._dailyWithdrawalLimit = dailyWithdrawalLimit
-        this._withdrawnToday = 0;
-        this._lastWithdrawalDate = new Date().getDate();
-    }
-
-    get accountNumber() {
-        return this._accountNumber;
-    }
-
-    get firstName () {
-        return this._firstName;
-    }
-
-    get lastName () {
-        return this._lastName;
-    }
-
-    get accountHolder () {
-        return this._accountHolder;
-    }
-
-    get balance () {
-        return this._balance;
-    }
-
-    deposit(amount) {
-        if (amount <= 10) {
-            throw new Error('Invalid amount entered. Minimum depost is 10');
-        }
-        else {
-            this._balance += amount;
-            this._transactions.push(new Transaction('Deposit', amount));
-            return `${this._accountHolder}, ${amount} has been deposited to your account. Your new balance is ${this._balance}`
-        }
-    }
-    
-    withdraw(amount) {
-        const today = new Date().getDate();
-
-        // Checks if its a new day and reset amount withdrawn today if necessary
-        if (today !== this._lastWithdrawalDate){
-            this._withdrawnToday = 0;
-            this._lastWithdrawalDate = today;
-        }
-
-        // Checks if there is enough balance for withdrawal
-        if (amount > this._balance) {
-            throw new Error('Insufficient funds');
-        }
-
-        // Checks if the amount exceeds the daily withdrawal limit
-        if ((this._withdrawnToday + amount) > this._dailyWithdrawalLimit) {
-            throw new Error('Exceeds Daily Withdrawal Limit')
-        }
-
-        this._balance -= amount;
-        this._withdrawnToday += amount;
-        this._transactions.push(new Transaction('Withdrawal', amount));
-        return `${this._accountHolder}, ${amount} has been withdrawn from your account. Your new balance is ${this._balance}`
-       
-    }
-
-    transfer(amount, recipientAccount) {
-
-        // Checks if the amount is positive
-        if (amount < 0) {
-            throw new Error('Invalid transfer amount');
-        }
-        
-        // Checks if there is enough balance for transfer
-        if (amount > this._balance) {
-            throw new Error('Insufficient funds');
-        }
-
-        // Checks if the amount exceeds the daily withdrawal limit
-        if ((this._withdrawnToday + amount) > this._dailyWithdrawalLimit) {
-            throw new Error('Exceeds Daily Withdrawal Limit')
-        }
-
-        this._balance -= amount;
-        this._withdrawnToday += amount;
-        recipientAccount.deposit(amount);
-        this._transactions.push(new Transaction('Transfer', amount));
-        return `You transferred ${amount} from this ${this._accountNumber} to ${recipientAccount.accountNumber}.`;
-    }
-
-    getTransactions() {
-        console.log(`Transaction History of ${this._accountHolder}`);
-        this._transactions.forEach(transaction => {
-            // console.log(`Type: ${transaction.type}, Amount: ${transaction.amount}, Timestamp: ${transaction.timestamp}`)
-            console.log(`${transaction.type} of ${transaction.amount} at ${transaction.timestamp}`)
-        });
-    }
-}
-
 class Transaction  {
     constructor(type, amount) {
         this._type = type;
@@ -119,32 +16,137 @@ class Transaction  {
     get timestamp () {
         return this._timestamp;
     }
+
+    resJson() {
+        return {
+            type: this._type,
+            amount: this._amount,
+            timestamp: this._timestamp
+        };
+    }
+}
+class BankAccount {
+    constructor(accountNumber, firstName, lastName, dailyWithdrawal) {
+        this._accountNumber = accountNumber;
+        this._firstName = firstName;
+        this._lastName = lastName;
+        this._accountHolder = `${firstName} ${lastName}`;
+        this._balance = 0;
+        this._transactions = [];
+        this._dailyWithdrawal = dailyWithdrawal;
+    }
+
+    get accountNumber() {
+        return this._accountNumber;
+    }
+
+    get accountHolder () {
+        return this._accountHolder;
+    }
+
+    get balance () {
+        return this._balance;
+    }
+
+    deposit(amount) {
+        if (amount <= 10) {
+            throw new Error('Invalid amount entered. Minimum deposit is 10');
+        }
+        else {
+            this._balance += amount;
+            this._transactions.push(new Transaction('Deposit', amount));
+            return `${this._accountHolder}, ${amount} has been deposited to your account. Your new balance is ${this._balance}`
+        }
+    }
+    getDailyWithdrawalTotal() {
+        const currentDate = new Date();
+        let total = 0;
+
+        this._transactions.forEach(transaction => {
+            if (transaction.timestamp.getDate() === currentDate.getDate()){
+                total += transaction.amount;
+            }
+        });
+        return total;
+    }
+
+    withdraw(amount) {
+        // Checks daily withdrawal limit
+        if (this.getDailyWithdrawalTotal() >= this._dailyWithdrawal) {
+            return 'Daily withdrawal limit exceeded';
+        }
+
+        // Checks if there is enough balance for withdrawal
+        if (amount > this._balance) {
+            return 'Insufficient funds';
+        }
+
+        this._balance -= amount;
+        this._transactions.push(new Transaction('Withdrawal', amount));
+        return `${this._accountHolder}, ${amount} has been withdrawn from your account. Your new balance is ${this._balance}`;
+       
+    }
+
+    transfer(amount, recipientAccount) {
+        // Checks if the amount is positive
+        if (amount < 0) {
+            throw new Error('Invalid transfer amount');
+        }
+
+        // Checks daily withdrawal limit
+        if (this.getDailyWithdrawalTotal() >= this._dailyWithdrawal) {
+            throw new Error('Daily withdrawal limit exceeded')
+        }
+        
+        // Checks if there is enough balance for transfer
+        if (amount > this._balance) {
+            throw new Error('Insufficient funds');
+        }
+
+        this._balance -= amount;
+        recipientAccount.deposit(amount);
+        this._transactions.push(new Transaction('Transfer', amount));
+        return `You transferred ${amount} from this ${this._accountNumber} to ${recipientAccount.accountNumber}.`;
+    }
+
+    getTransactions() {
+        let transactions = [];
+
+        this._transactions.forEach(transaction => {
+            transactions.push(transaction.resJson());
+        });
+
+        return transactions;
+    }
 }
 
-const client1 = new BankAccount(3035682914,"Jane", "Doe", 20000, 10000);
-const client2 = new BankAccount(7701770147, "John", "Doe", 1000, 500);
+TIER1 = 2000;
+
+const client1 = new BankAccount("3035682914","Jane", "Doe", TIER1);
+const client2 = new BankAccount("7701770147", "John", "Doe", TIER1);
 
 
 console.log('Account Number:', client1.accountNumber);
 console.log('Account Name:', client1.accountHolder);
-console.log('Account Balance:', client1.balance);
-
-console.log(client1.balance)
-console.log(client2.balance)
-
 
 
 try {
-    client2.deposit(2000);
-    client1.withdraw(5000);
-    client1.transfer(5000, client2);
-    client2.transfer(100, client1)
+    console.log(client1.deposit(5000));
+    console.log(client2.deposit(3000));
+
+    client1.transfer(200, client2);
+    client2.transfer(300, client1);
+    
 } catch (error) {
     console.log(error.message);
 }
 
-client1.getTransactions();
-client2.getTransactions();
+console.log(client1.withdraw(500));
+console.log(client1.withdraw(1000));
+console.log(client2.withdraw(500));
 
-console.log(client1.balance)
-console.log(client2.balance)
+console.log('Account 1 Balance:', client1.balance);
+console.log('Account 2 Balance:', client2.balance);
+
+console.log(client1.getTransactions());
+console.log(client2.getTransactions());
